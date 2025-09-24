2834546b36d2057243dc3036ef68f56fda3bc808
Serhii <grey.egorov@gmail.com>
2025-09-24 13:08:03 +0300

    виправлення кнопки

--- src/scripts/services/telegram.service.js
+++ src/scripts/services/telegram.service.js
@@ -5,31 +5,30 @@ export function initializeTelegramWebApp() {
         AppState.tg = window.Telegram?.WebApp;
         if (AppState.tg) {
             AppState.tg.ready();
-            AppState.tg.expand();
-            document.getElementById('order-button-container').style.display = 'none';
+            AppState.tg.expand();                    
         } else {
-            
+            document.getElementById('order-button-container').style.display = 'none'; 
             throw new Error("Telegram WebApp is not available");
         }
     } catch (e) {
         console.warn(e.message);
-        // Mock object for browser testing
-        AppState.tg = {
-            initData: 'dev_mode_init_data',
-            initDataUnsafe: { user: { id: 'demo_user', first_name: 'Test', last_name: 'User' } },
-            MainButton: {
-                setText: (text) => console.log('TG MainButton Text:', text),
-                show: () => console.log('TG MainButton: Show'),
-                hide: () => console.log('TG MainButton: Hide'),
-                setParams: (params) => console.log('TG MainButton Params:', params),
-            },
-            BackButton: {
-                show: () => console.log('TG BackButton: Show'),
-                hide: () => console.log('TG BackButton: Hide'),
-            },
-            onEvent: (event, callback) => console.log('TG Event Registered:', event, callback),
-            close: () => console.log('TG Close'),
-            showAlert: (message) => alert(`[TG]: ${message}`),
-        };
+        // // Mock object for browser testing
+        // AppState.tg = {
+        //     initData: 'dev_mode_init_data',
+        //     initDataUnsafe: { user: { id: 'demo_user', first_name: 'Test', last_name: 'User' } },
+        //     MainButton: {
+        //         setText: (text) => console.log('TG MainButton Text:', text),
+        //         show: () => console.log('TG MainButton: Show'),
+        //         hide: () => console.log('TG MainButton: Hide'),
+        //         setParams: (params) => console.log('TG MainButton Params:', params),
+        //     },
+        //     BackButton: {
+        //         show: () => console.log('TG BackButton: Show'),
+        //         hide: () => console.log('TG BackButton: Hide'),
+        //     },
+        //     onEvent: (event, callback) => console.log('TG Event Registered:', event, callback),
+        //     close: () => console.log('TG Close'),
+        //     showAlert: (message) => alert(`[TG]: ${message}`),
+        // };
     }
 }
