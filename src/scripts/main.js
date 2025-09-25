7e8151edb9e17525908af1c4591aa0a212bc4d1f
Serhii <grey.egorov@gmail.com>
2025-09-25 14:54:11 +0300

    Виправлення кнопки

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -83,6 +83,20 @@ function initApp() {
         console.log('✅ App initialized successfully');
     });
 
+        document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById('order-button-container');
+
+    if (window.Telegram && window.Telegram.WebApp) {
+        // Якщо відкрито через Telegram WebApp, приховуємо кнопку
+        container.style.display = 'none';
+        console.log("Telegram WebApp – кнопка прихована");
+    } else {
+        // Веб/інший браузер – показуємо кнопку
+        container.style.display = 'block';
+        console.log("Не Telegram WebApp – кнопка показана");
+    }
+});
+
 
 }
 
