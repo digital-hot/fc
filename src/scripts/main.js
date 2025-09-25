a4d8278258e597cf2b361b4017e89e6ddaaaf963
Serhii <grey.egorov@gmail.com>
2025-09-25 15:10:05 +0300

    вк

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -82,19 +82,7 @@ function initApp() {
         updateUI();
         console.log('✅ App initialized successfully');
     });
-        document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById('order-button-container-1');
 
-    if (window.Telegram && window.Telegram.WebApp) {
-        // Якщо відкрито через Telegram WebApp, приховуємо кнопку
-        container.style.display = 'none';
-        console.log("Telegram WebApp – кнопка прихована");
-    } else {
-        // Веб/інший браузер – показуємо кнопку
-        container.style.display = 'block';
-        console.log("Не Telegram WebApp – кнопка показана");
-    }
-});
 
 }
 
